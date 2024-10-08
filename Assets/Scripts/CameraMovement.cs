0cad1395e15da033aa552d77d2b1c912dbc3c931
MarcoSeri <marcoseri2006@gmail.com>
2024-10-08 13:33:57 -0300

    Changed camera movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour{
     [SerializeField]private float velocidad = 1f;
     public bool DisplayMap = false;
+    private float currentDificult = 1;
     void Start(){
     }
 
     void FixedUpdate(){
     }
     public void MoveCamera(float dificulty){
-            transform.Translate(new Vector3(0,0,velocidad*dificulty*Time.deltaTime), Space.World);
+        if (dificulty != currentDificult)
+        {
+            currentDificult += 0.5f;
+            transform.Translate(new Vector3(0,0,velocidad*currentDificult*Time.deltaTime), Space.World);
+        }
+        else if(dificulty == currentDificult)
+            transform.Translate(new Vector3(0, 0, velocidad * dificulty * Time.deltaTime), Space.World);
     }
 
     public void OnTriggerEnter(Collider other)
